223dcd96a776c519e5815282fefaa67e634449d5
Mitch Thompson <thomitchell@gmail.com>
2016-07-29 15:27:50 -0700

    Adds assignment 5
    REST APIs

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
-var terms = require('./lib/techTerms.js');
 
 //express setup
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/../public'));
 app.use(require("body-parser").urlencoded({extended: true}));
+app.use('/api', require('cors')()); // set Access-Control-Allow-Origin header for api route
 
 //handlebars setup
 var handlebars = require('express-handlebars').create({
@@ -15,94 +15,7 @@ var handlebars = require('express-handlebars').create({
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-var goBack = "<br><br><a href='/'>Back</a>";
-var allLink = "<br><br><a href='/all'>See all tech terms</a>";
-
-app.get('/', function(req, res){
-    res.type('text/html');
-    res.render('home', {
-        title: 'Home',
-        terms: terms.allTechTerms()
-    });
-});
-
-app.get('/detail/:term', function(req,res){
-    res.type('text/html');
-    var found = terms.getTechTerm(req.params.term);
-    res.render('detail', {
-        title: 'Detail',
-        term: found
-    });    
-});
-
-app.get('/about', function(req, res){;
-    res.send('about');
-});
-
-app.get('/all', function(req,res){
-    res.type('text/html');
-    var header = '<h1>List of all tech terms</h1>';
-    var result =  terms.allTechTerms();
-    res.send(header + result + goBack);
-});
-
-app.post('/search', function(req,res){
-    res.type('text/html');
-    var search = terms.getTechTerm(req.body.term);
-    if(!search){
-        res.render('detail', {
-            title: 'Detail',
-            result: true,
-            term: req.body.term
-        });
-    }else {
-        res.render('detail', {
-            title: 'Detail',
-            term: search
-        });
-    }  
-});
-
-app.post('/update', function(req,res){
-    res.type('text/html');
-    var update =  terms.updateTechTerm(req.body.term, req.body.defined);
-    var found = terms.getTechTerm(req.body.term);
-    res.render('detail', {
-        title: 'Detail',
-        term: found,
-        updated: true
-    });
-});
-
-app.post('/add', function(req,res){
-    res.type('text/html');
-    var add =  terms.addTechTerm(req.body.term, req.body.defined);
-    if(!add){
-        res.render('detail', {
-            title: 'Detail',
-            notAdded: true,
-            term: req.body.term
-        });
-    }else {
-        var newTerm = terms.getTechTerm(add); 
-        res.render('detail', {
-            title: 'Detail',
-            added:true,
-            term: newTerm
-        });
-    }  
-});
-
-app.post('/delete', function(req,res){
-    res.type('text/html');
-    var removed =  terms.removeTechTerm(req.body.term);
-    res.render('home', {
-        title: 'Home',
-        removed: true,
-        terms: terms.allTechTerms()
-    });
-});
-
+var routes = require('./lib/routes.js')(app);
 
 //404 catch-all handler
 app.use(function(req,res){
@@ -119,6 +32,6 @@ app.use(function(err, req, res, next){
 });
 
 app.listen(app.get('port'), function(){
-    console.log('Express started on http:/localhost:' +
-               app.get('port') + '; press Ctrl-C to terminate.');
+console.log('Express started on http:/localhost:' +
+           app.get('port') + '; press Ctrl-C to terminate.');
 });
